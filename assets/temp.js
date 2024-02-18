e4d373cef9a29457c0d388342ba5b814c7402973
Roberto Marsella <roberto.marsella@icloud.com>
2024-02-18 23:02:12 +0000

    Added button to download cv. [Need to pug backend and work out icon hover]

--- assets/temp.js
+++ assets/temp.js
@@ -1,51 +1,51 @@
-function ap() {
+function bd() {
+    const {title: e, description: t, experiences: n, education: k} = dc;
     return v.jsxs("div", {
-        className: "projects-container",
+        className: "about-container",
         children: [v.jsx("h2", {
-            className: "projects-headline",
-            children: Ju.title
+            className: "about-title",
+            children: e
+        }), 
+        v.jsxs("p", {
+            className: "about-description",
+            children: [t, 
+            //     " Follow my ", 
+            //     v.jsx("a", {
+            //     className: "instagram",
+            //     href: "https://www.instagram.com/mirayatech",
+            //     target: "_blanket",
+            //     children: "Instagram"
+            // }), " to see my journey."
+            ]
+        }),
+        v.jsx(Jd, {}), v.jsxs("div", {
+            className: "about-experience-section",
+            children: [v.jsx("h2", {
+                className: "about-experience-heading",
+                children: "Work Experience"
+            }), n.map((r,l)=>v.jsx(qd, {
+                date: r.date,
+                title: r.title,
+                company: r.company,
+                description: r.description,
+                technologies: r.technologies
+            }, l))],
+        }), v.jsxs("div", {
+            className: "about-experience-section",
+            children: [v.jsx("h2", {
+                className: "about-experience-heading",
+                children: "Education"
+            }), k.map((r,l)=>v.jsx(edd, {
+                date: r.date,
+                title: r.title,
+                uni: r.uni,
+                description: r.description
+            }, l))],
         }), v.jsx("div", {
-            className: "projects-wrapper",
-            children: Ju.projects.map(e=>v.jsxs("a", {
-                href: e.github,
-                className: "projects-card",
-                target: "_blank",
-                children: [v.jsx("div", {
-                    className: "bar",
-                    children: [ v.jsx("div", {
-                        className: "red"
-                    }),v.jsx("div", {
-                        className: "yellow"
-                    }),v.jsx("div", {
-                        className: "green"
-                    })]
-                }), v.jsx("div", {
-                    className: "screen",
-                    children: [// v.jsx("div", {
-                        //     className: "projects-card-gardient"
-                        // }), 
-                        v.jsxs("div", {
-                            className: "projects-card-links",
-                            children: [] // [v.jsx("a", {
-                            //     href: e.github,
-                            //     target: "_blank",
-                            //     children: v.jsx(hc, {})
-                            // }), v.jsx("a", {
-                            //     href: e.live,
-                            //     target: "_blank",
-                            //     children: v.jsx(op, {})
-                            // })]
-                        }), v.jsx("h3", {
-                            className: "projects-card-name",
-                            children:[] // e.name
-                        }), v.jsx("div", {
-                            className: "projects-card-languages",
-                            children: [] // e.languages.map(t=>v.jsx("span", {
-                                // children: t
-                            // }, t))
-                        })]
-                })]
-            }, e.name))
+            className: "about-experience-section download-button",
+            children: [v.jsx("p", {
+                children: "Download my CV here"
+            }), v.jsx(dw, {})]
         })]
     })
 }
